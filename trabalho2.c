4f4a435edf4c4b8c5328a616eec7dd23f5080aba
Bárbara Pegoraro Markus <30914374+barbs-pm@users.noreply.github.com>
2017-11-16 16:34:06 -0200

    Trabalho 2- Triângulo C

--- trabalho2.c
+++ trabalho2.c
@@ -1,56 +1,75 @@
+/*
+*  ============================
+*  Anthony Nadaletti
+*  https://github.com/SrAnthony
+*  Bárbara Pegoraro
+*  https://github.com/barbs-pm
+*  ============================
+*
+*  Exemplo de triângulo:
+*  a(25,45) | b(5,25) | c(45,5)
+*
+*/
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio_ext.h>
+#include <math.h>
 typedef struct{
-	float x;
-	float y;
+	int x, y;
+}Ponto;
 
-} points;
-
-float sinal (points point_a, points point_b, points point_c){
-    int resul=(point_a.x - point_c.x) * (point_b.y - point_c.y) - (point_b.x - point_c.x) * (point_a.y - point_c.y);
-    return resul;
-}
-
-int pontoNoTriangulo(points p, points a, points b, points c){
-    int v1, v2, v3;
-
-    v1 = sinal(p, a, b) < 0.0f;
-    v2 = sinal(p, b, c) < 0.0f;
-    v3 = sinal(p, c, a) < 0.0f;
-
-    if(((v1 == v2) && (v2 == v3)))
-    	return 1;
-
-    else
-    	return 0;
+int area(Ponto a, Ponto b, Ponto c){
+	return ((a.x * b.y) + (a.y * c.x) + (b.x * c.y)) - ((c.x * b.y) + (b.x * a.y) + (a.x * c.y));
 }
 
 int main(){
-	points a, b, c, p;
-	puts("Digite o x de a");
-	scanf("%f", &a.x);
-	puts("Digite o y de a");
-	scanf("%f", &a.y);
-	puts("Digite o x de b");
-	scanf("%f", &b.x);
-	puts("Digite o y de b");
-	scanf("%f", &b.y);
-	puts("Digite o x de c");
-	scanf("%f", &c.x);
-	puts("Digite o y de c");
-	scanf("%f", &c.y);
-	puts("Digite o x do ponto");
-	scanf("%f", &p.x);
-	puts("Digite o y do ponto");
-	scanf("%f", &p.y);
-	
-	if(pontoNoTriangulo(p, a, b, c)){
-		printf("OK\n");
-	}
-	else{
-		printf("FALSO\n");
-	}
+	Ponto q, a, b, c;
+	int i, j, AB, AC, BC;
+	system("clear");
+	printf("   --- 50 x 50 ---\n");
+	printf("-| Informações do primeiro ponto (x,y)\n-| ");
+	scanf("%d %d",&a.x, &a.y);
 
+	printf("-| Informações do segundo ponto (x,y)\n-| ");
+    scanf("%d %d",&b.x, &b.y);
+
+	printf("-| Informações do terceiro ponto (x,y)\n-| ");
+    scanf("%d %d",&c.x, &c.y);
+	
+	int tam = area(a, b, c);
+	system("clear");
+	printf("                   \e[1mA área é: %d\e[0m\n  ", tam <= 0 ? -tam : tam);
+	for(i = 0; i < 50; i++) printf("_");
+	printf("\n |      a(%2d,%2d)   |   b(%2d,%2d)   |   c(%2d,%2d)      |\n |", a.x, a.y, b.x, b.y, c.x, c.y);
+	for(i = 0; i < 50; i++) printf("‾");
+	printf("|\n ");
+
+	for(i = 0; i < 50; i++){
+		printf("|");
+		for(j = 0; j < 50; j++){
+
+			q.x = i;
+			q.y = j;
+
+			AB = area(a, b, q);
+			if(AB <= 0) AB = -AB;
+		
+			AC = area(a, c, q);
+			if(AC <= 0) AC = -AC;
+			
+			BC = area(b, c, q);
+			if(BC <= 0) BC = -BC;
+
+			if(AB + AC + BC <= tam)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("|\n ");
+	}
+	printf(" ");
+	for(i = 0; i < 50; i++)
+		printf("‾");
+	printf("\n");
 	return 0;
 }
